96c0ce89b279070d62728ab4212b298361a67774
Seean Kim <kim.seean@gmail.com>
2015-07-01 18:34:20 -0400

    fixes to main header for large screens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,9 +25,9 @@ $(function() {
   });
 });
 
-var bg = jQuery("#bg1, #bg2");
-jQuery(window).resize("resizeBackground");
-function resizeBackground() {
-    bg.height(jQuery(window).height() + 60);
-}
-resizeBackground();
+// var bg = jQuery("#bg1, #bg2");
+// jQuery(window).resize("resizeBackground");
+// function resizeBackground() {
+//     bg.height(jQuery(window).height() + 60);
+// }
+// resizeBackground();
