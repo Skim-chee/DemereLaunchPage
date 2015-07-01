473fe9318b93cde169f986cb9aa734c6d71eb384
Seean Kim <kim.seean@gmail.com>
2015-07-01 18:18:24 -0400

    mobile bug fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,10 @@ $(function() {
     }, 1000);
   });
 });
+
+var bg = jQuery("#bg1, #bg2");
+jQuery(window).resize("resizeBackground");
+function resizeBackground() {
+    bg.height(jQuery(window).height() + 60);
+}
+resizeBackground();
