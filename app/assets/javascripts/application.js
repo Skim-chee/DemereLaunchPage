91a3875d3ef901786baa3cf05d3cfe82c2e66386
Seean Kim <kim.seean@gmail.com>
2015-07-01 18:42:49 -0400

    returned sign up scroll to top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,10 +24,3 @@ $(function() {
     }, 1000);
   });
 });
-
-// var bg = jQuery("#bg1, #bg2");
-// jQuery(window).resize("resizeBackground");
-// function resizeBackground() {
-//     bg.height(jQuery(window).height() + 60);
-// }
-// resizeBackground();
