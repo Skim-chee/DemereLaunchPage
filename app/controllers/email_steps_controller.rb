dbaf5c8311bf6aee80c4be4a13a613f4020858ce
Seean Kim <kim.seean@gmail.com>
2015-07-06 14:58:00 -0400

    changed so that user saves with just email address

--- app/controllers/email_steps_controller.rb
+++ app/controllers/email_steps_controller.rb
@@ -8,40 +8,9 @@ class EmailStepsController < ApplicationController
   end
 
   def update
-    @email = Email.new(info_params)
-    @email.email = session[:email]
+    @email = Email.find_by(session[:id])
+    @email.update_attributes(info_params)
     if @email.valid?(:second)
-      current_ip = IpAddress.find_by_address(request.remote_ip)
-
-			if !current_ip
-				current_ip = IpAddress.create(
-					:address => request.remote_ip,
-					:count => 0
-				)
-			end
-
-			if current_ip.count > 9
-				flash[:danger] = "Too many accounts are already linked to this ip address"
-				return redirect_to root_url
-			else
-			current_ip.count = current_ip.count + 1
-			current_ip.save
-			end
-
-			if current_ip.count < 10
-				@referred_by = Email.find_by_referral_code(cookies[:h_ref])
-			end
-
-			puts '----TESTING----'
-			puts @referred_by.email if @referred_by
-			puts request.remote_ip.inspect
-			puts current_ip.count
-			puts '----END TESTING----'
-
-			if !@referred_by.nil?
-				@email.referrer = @referred_by
-			end
-      @email.save
       render 'emails/refer'
       puts "GOOD TO GO"
     else
--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -8,6 +8,36 @@ class EmailsController < ApplicationController
 		@em = Email.new(email_params)
 		if @em.valid?(:first)
 			@email = Email.find_by(email_params)
+			current_ip = IpAddress.find_by_address(request.remote_ip)
+
+			if !current_ip
+				current_ip = IpAddress.create(
+					:address => request.remote_ip,
+					:count => 0
+				)
+			end
+
+			if current_ip.count > 9
+				flash[:danger] = "Too many accounts are already linked to this ip address"
+				return redirect_to root_url
+			else
+			current_ip.count = current_ip.count + 1
+			current_ip.save
+			end
+
+			if current_ip.count < 10
+				@referred_by = Email.find_by_referral_code(cookies[:h_ref])
+			end
+
+			puts '----TESTING----'
+			puts @referred_by.email if @referred_by
+			puts request.remote_ip.inspect
+			puts current_ip.count
+			puts '----END TESTING----'
+
+			if !@referred_by.nil?
+				@em.referrer = @referred_by
+			end
 		else
 			puts "TESTING ERRORR"
 			if @em.errors.any?
@@ -20,7 +50,7 @@ class EmailsController < ApplicationController
 		# flash success message, and redirect to home page
 		if !@email
 			# @email.send_confirmation
-				session[:email] = @em.email
+				@em.save
 				session[:id] = @em.id
 				puts "THIS MAH SESSION ID"
 				puts session[:id]
