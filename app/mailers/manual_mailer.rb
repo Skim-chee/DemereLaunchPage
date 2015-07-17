68d061a1f76374cf5e9c70684724e7dfbe021a3e
Seean Kim <kim.seean@gmail.com>
2015-07-16 23:52:43 -0400

    dammit 7day email

--- app/mailers/manual_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ManualMailer < ApplicationMailer
-	default from: "demere@demere.co"
-
-	def manual_email(email)
-		@email = email
-		mail(to: @email.email, subject: 'Thank you for signing up!')
-	end
-end
--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,5 +1,6 @@
-class WelcomeMailer < ApplicationMailer
+class WelcomeMailer < ActionMailer::Base
 	default from: "demere@demere.co"
+	layout 'welcome_mailer'
 
 	def welcome_email(email)
 		@email = email
